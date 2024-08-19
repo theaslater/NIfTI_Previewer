b06136ea7e8be309cea1986dd18eef7f37c73887
theaslater <slater.theodora@gmail.com>
2024-08-19 17:11:25 +0100

    Added keyboad support to scroll through images

--- preview_nifti.py
+++ preview_nifti.py
@@ -10,30 +10,59 @@ import sys
 import os
 import nibabel as nib
 import matplotlib.pyplot as plt
+import time
 
 class Interact:
     def __init__(self, data, dims, axes):
         self.data = data
         self.dims = dims
         self.axes = axes
-        self.sagittal_index = data.shape[0] // 2
-        self.coronal_index = data.shape[1] // 2
-        self.axial_index = data.shape[2] // 2
+        self.image_index = [data.shape[0] // 2, data.shape[1] // 2, data.shape[2] // 2]
+        self.last_clicked = 2  # Default movement in the Axial plane
+        self.last_keypress_time = 0
+        self.keypress_interval = 0.1
+
+        # Initial display and title setup
+        self.update_images()
 
     def update_on_click(self, click):
         if click.inaxes == self.axes[0]:  # Sagittal slice clicked
-            self.coronal_index = int(click.xdata)
-            self.axial_index=int(click.ydata)
+            self.image_index[1] = int(click.xdata)
+            self.image_index[2] = int(click.ydata)
+            self.last_clicked = 0
         elif click.inaxes == self.axes[1]:  # Coronal slice clicked
-            self.sagittal_index = int(click.xdata)
-            self.axial_index=int(click.ydata)
+            self.image_index[0] = int(click.xdata)
+            self.image_index[2] = int(click.ydata)
+            self.last_clicked = 1
         elif click.inaxes == self.axes[2]:  # Axial slice clicked
-            self.sagittal_index=int(click.xdata)    
-            self.coronal_index = int(click.ydata)
-        # Update the images
-        self.axes[0].imshow(self.data[self.sagittal_index, :, :].T, cmap='gray', origin='lower', aspect=self.dims[3]/self.dims[1])
-        self.axes[1].imshow(self.data[:, self.coronal_index, :].T, cmap='gray', origin='lower', aspect=self.dims[3]/self.dims[2])
-        self.axes[2].imshow(self.data[:, :, self.axial_index].T, cmap='gray', origin='lower', aspect=self.dims[1]/self.dims[2]) # If image is ever squashed, reverse this first!
+            self.image_index[0] = int(click.xdata)
+            self.image_index[1] = int(click.ydata)
+            self.last_clicked = 2
+        self.update_images()
+
+    def update_on_key(self, event):
+        current_time = time.time()
+        if current_time - self.last_keypress_time < self.keypress_interval:
+            return
+
+        self.last_keypress_time = current_time
+        
+        key_direction = (event.key, self.last_clicked)
+        if key_direction in DIRECTION_MAP:
+            idx, op = DIRECTION_MAP[key_direction]
+            self.image_index[idx] = max(0, min(self.image_index[idx] + op, self.data.shape[idx] - 1))
+            self.update_images()
+
+    def update_images(self):
+        self.axes[0].imshow(self.data[self.image_index[0], :, :].T, cmap='gray', origin='lower', aspect=self.dims[3]/self.dims[1])
+        self.axes[1].imshow(self.data[:, self.image_index[1], :].T, cmap='gray', origin='lower', aspect=self.dims[3]/self.dims[2])
+        self.axes[2].imshow(self.data[:, :, self.image_index[2]].T, cmap='gray', origin='lower', aspect=self.dims[1]/self.dims[2])
+        self.update_titles()
+
+    def update_titles(self):
+        self.axes[0].set_title(f'Sagittal [{self.image_index[0]}]')
+        self.axes[1].set_title(f'Coronal [{self.image_index[1]}]')
+        self.axes[2].set_title(f'Axial [{self.image_index[2]}]')
         plt.draw()
 
 def preview_nifti(filename):
@@ -43,28 +72,34 @@ def preview_nifti(filename):
     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
     dims = nii.header['pixdim']
     
-    print(nii.header)
     viewer = Interact(data, dims, axes)
-    
-    # Initial display
-    axes[0].imshow(data[viewer.sagittal_index, :, :].T, cmap='gray', origin='lower', aspect=dims[3]/dims[1])
-    axes[1].imshow(data[:, viewer.coronal_index, :].T, cmap='gray', origin='lower', aspect=dims[3]/dims[2])
-    axes[2].imshow(data[:, :, viewer.axial_index].T, cmap='gray', origin='lower', aspect=dims[1]/dims[2])
     
     plt.suptitle(f"{os.path.basename(filename)}")
     plt.tight_layout()
-    
-    # Turn off axes and set titles
+
     axes[0].axis('off')
-    axes[0].set_title('Sagittal')
     axes[1].axis('off')
-    axes[1].set_title('Coronal')
     axes[2].axis('off')
-    axes[2].set_title('Axial')
-    # Connect the click event to the viewer's update method
+
     fig.canvas.mpl_connect('button_press_event', viewer.update_on_click)
+    fig.canvas.mpl_connect('key_press_event', viewer.update_on_key)
     
     plt.show()
+
+DIRECTION_MAP = {
+    ('left', 0): (1, -1),
+    ('left', 1): (0, -1),
+    ('left', 2): (0, -1),
+    ('right', 0): (1, 1),
+    ('right', 1): (0, 1),
+    ('right', 2): (0, 1),
+    ('up', 0): (2, 1),
+    ('up', 1): (2, 1),
+    ('up', 2): (1, 1),
+    ('down', 0): (2, -1),
+    ('down', 1): (2, -1),
+    ('down', 2): (1, -1),
+}
 
 
 if len(sys.argv) > 1:
