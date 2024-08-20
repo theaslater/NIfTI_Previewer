87148c707e413dd89a40f470c04e7f8e72c1ed0b
theaslater <slater.theodora@gmail.com>
2024-08-20 09:28:33 +0100

    Added comments detailing code

--- preview_nifti.py
+++ preview_nifti.py
@@ -14,46 +14,49 @@ import time
 
 class Interact:
     def __init__(self, data, dims, axes):
+        # Initialize all nessessary variables
         self.data = data
         self.dims = dims
         self.axes = axes
+        # Select middle slice on each axis as default
         self.image_index = [data.shape[0] // 2, data.shape[1] // 2, data.shape[2] // 2]
         self.last_clicked = 2  # Default movement in the Axial plane
         self.last_keypress_time = 0
-        self.keypress_interval = 0.1
+        self.keypress_interval = 0.2 # Delay for preventing excessive key press input
 
         # Initial display and title setup
         self.update_images()
 
     def update_on_click(self, click):
         if click.inaxes == self.axes[0]:  # Sagittal slice clicked
-            self.image_index[1] = int(click.xdata)
-            self.image_index[2] = int(click.ydata)
-            self.last_clicked = 0
+            self.image_index[1] = int(click.xdata) # Change plane in Coronal
+            self.image_index[2] = int(click.ydata) # Change plane in Axial
+            self.last_clicked = 0 # Changes which view is changed upon an arrow key press
         elif click.inaxes == self.axes[1]:  # Coronal slice clicked
-            self.image_index[0] = int(click.xdata)
-            self.image_index[2] = int(click.ydata)
-            self.last_clicked = 1
-        elif click.inaxes == self.axes[2]:  # Axial slice clicked
-            self.image_index[0] = int(click.xdata)
-            self.image_index[1] = int(click.ydata)
-            self.last_clicked = 2
+            self.image_index[0] = int(click.xdata) # Change plane is Sagittal
+            self.image_index[2] = int(click.ydata) # Change plane is Axial
+            self.last_clicked = 1 # Changes which view is changed upon an arrow key press
+        elif click.inaxes == self.axes[2]: # Axial slice clicked
+            self.image_index[0] = int(click.xdata) # Change plane in Sagittal
+            self.image_index[1] = int(click.ydata) # Change plane in Coronal
+            self.last_clicked = 2 # Changes which view is changed upon an arrow key press
         self.update_images()
 
     def update_on_key(self, event):
-        current_time = time.time()
+        # Drop out of update_on_key() if keys are pressed too rapidly
+        current_time = time.time() 
         if current_time - self.last_keypress_time < self.keypress_interval:
             return
-
         self.last_keypress_time = current_time
         
-        key_direction = (event.key, self.last_clicked)
-        if key_direction in DIRECTION_MAP:
-            idx, op = DIRECTION_MAP[key_direction]
-            self.image_index[idx] = max(0, min(self.image_index[idx] + op, self.data.shape[idx] - 1))
-            self.update_images()
+        
+        key_direction = (event.key, self.last_clicked) # Contains which key was last pressed and which view was last clicked
+        if key_direction in DIRECTION_MAP: # If a valid direction within the dictionary
+            idx, op = DIRECTION_MAP[key_direction] # idx contains which image needs updating, op contains the direction being changed
+            self.image_index[idx] = max(0, min(self.image_index[idx] + op, self.data.shape[idx] - 1)) #update the image index for the relevant image
+            self.update_images()# Update images
 
-    def update_images(self):
+    def update_images(self): # Update all images - Could be made more efficient if only relevant images are updates
         self.axes[0].imshow(self.data[self.image_index[0], :, :].T, cmap='gray', origin='lower', aspect=self.dims[3]/self.dims[1])
         self.axes[1].imshow(self.data[:, self.image_index[1], :].T, cmap='gray', origin='lower', aspect=self.dims[3]/self.dims[2])
         self.axes[2].imshow(self.data[:, :, self.image_index[2]].T, cmap='gray', origin='lower', aspect=self.dims[1]/self.dims[2])
@@ -66,26 +69,27 @@ class Interact:
         plt.draw()
 
 def preview_nifti(filename):
-    nii = nib.load(filename)
-    data = nii.get_fdata()
+    nii = nib.load(filename) # Load in NIfTI
+    data = nii.get_fdata() # Extract main image data
     
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-    dims = nii.header['pixdim']
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5)) # Define image area
+    dims = nii.header['pixdim'] # Extract dimentions of pixels
     
-    viewer = Interact(data, dims, axes)
+    viewer = Interact(data, dims, axes) # Get initial values from Interact class
     
+    # Display area settings
     plt.suptitle(f"{os.path.basename(filename)}")
     plt.tight_layout()
-
     axes[0].axis('off')
     axes[1].axis('off')
     axes[2].axis('off')
 
+    # Update viewer values upon a click or button press
     fig.canvas.mpl_connect('button_press_event', viewer.update_on_click)
     fig.canvas.mpl_connect('key_press_event', viewer.update_on_key)
-    
     plt.show()
 
+# Dictionary loaded as global to prevent it being loaded every time the user interacts with the images
 DIRECTION_MAP = {
     ('left', 0): (1, -1),
     ('left', 1): (0, -1),
@@ -101,14 +105,12 @@ DIRECTION_MAP = {
     ('down', 2): (1, -1),
 }
 
-
+# Check a file name was provided by command line arg
 if len(sys.argv) > 1:
     filename = sys.argv[1]
     try:
-        preview_nifti(filename)
-
+        preview_nifti(filename) # Preview the NIfTI file
     except Exception as e:
-        input(f"Error loading NIfTI file: {e}\n")
+        input(f"Error loading NIfTI file: {e}\n") # Error incase the preview goes wrong
 else:
-    with open("/home/thea/SCRIPTS/test_log.txt", "a") as log_file:
-        input("No file specified.\n")
+    input("No file specified XO\n")
